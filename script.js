a8069c09adbafd76f2ecf88df97eb53cf415e28b
nika277 <131463211+nika277@users.noreply.github.com>
2024-01-16 17:37:19 +0400

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+console.log("Front-End Development");
+let name = "Nika";
+let surname = "Abuladze";
+let age = "18";
+console.log(name, surname, age);
+console.log(name + " " + surname + " is " + age + " Years Old ");
+let person = {
+    name: "Nick",
+    surname: "Walker",
+    age: "22",
+    isMarried: false,
+};
+console.log(`My name is ${person.name} ${person.surname} ${person.age} Years Old  Im ${person.isMarried}`);
+
+let x = 5;
+let y = 7;
+let z = x * y; 
+
+console.log(z); 
